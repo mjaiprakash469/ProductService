37fca01549a87d18ca3e57e85cc5776385448ff6
ajayranam1 <98026013+ajayranam1@users.noreply.github.com>
2022-02-07 15:51:52 +0530

    Update ProductRepository.java

--- src/main/java/com/ibuy/product/demo/repository/ProductRepository.java
+++ src/main/java/com/ibuy/product/demo/repository/ProductRepository.java
@@ -1,5 +1,8 @@
 package com.ibuy.product.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ibuy.product.demo.entity.Product;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 	Product findByProductId(int productId);
