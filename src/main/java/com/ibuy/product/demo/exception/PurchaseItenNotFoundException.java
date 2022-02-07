e8dd29366f181fd386e1c50d079aaf40c371eafc
ajayranam1 <98026013+ajayranam1@users.noreply.github.com>
2022-02-07 15:44:42 +0530

    Add files via upload

---
+++ src/main/java/com/ibuy/product/demo/exception/PurchaseItenNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ibuy.product.demo.exception;
+
+public class PurchaseItenNotFoundException extends RuntimeException {
+
+	public PurchaseItenNotFoundException(String msg) {
+		
+		super(msg);
+		
+	}
+
+}
