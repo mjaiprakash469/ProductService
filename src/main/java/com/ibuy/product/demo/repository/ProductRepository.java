b1a4539f115ffdd2e2a43e284e10875b8fe063a9
ajayranam1 <98026013+ajayranam1@users.noreply.github.com>
2022-02-07 15:49:53 +0530

    Update ProductRepository.java

--- src/main/java/com/ibuy/product/demo/repository/ProductRepository.java
+++ src/main/java/com/ibuy/product/demo/repository/ProductRepository.java
@@ -1,5 +1,11 @@
 package com.ibuy.product.demo.repository;
 
-public class ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+	Product findByProductId(int productId);
+
+	
+
+	
 
 }
