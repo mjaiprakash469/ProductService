011759d265f17ce787b9f7a7a522045e0fff2f1a
ajayranam1 <98026013+ajayranam1@users.noreply.github.com>
2022-02-07 15:42:07 +0530

    Update ProductControllerAdvice.java

--- src/main/java/com/ibuy/product/demo/advice/ProductControllerAdvice.java
+++ src/main/java/com/ibuy/product/demo/advice/ProductControllerAdvice.java
@@ -1,5 +1,18 @@
 package com.ibuy.product.demo.advice;
 
-public class ProductControllerAdvice {
+@ControllerAdvice
+public class ProductControllerAdvice extends ResponseEntityExceptionHandler{
+	
+	
+	
+	@ExceptionHandler(PurchaseItenNotFoundException.class)
+	public ResponseEntity<String> emtyHandler(PurchaseItenNotFoundException purchaseItenNotFoundException){
+		return new ResponseEntity<String>("Invalid Username or Password", HttpStatus.BAD_REQUEST);
+	}
 
-}
+
+
+	}
+
+
+
