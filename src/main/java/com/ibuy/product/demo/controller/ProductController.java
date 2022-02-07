8f5fae314c304c65a80fc74461e5ce0fddd6e8a7
ajayranam1 <98026013+ajayranam1@users.noreply.github.com>
2022-02-07 15:48:37 +0530

    Update ProductController.java

--- src/main/java/com/ibuy/product/demo/controller/ProductController.java
+++ src/main/java/com/ibuy/product/demo/controller/ProductController.java
@@ -1,5 +1,26 @@
 package com.ibuy.product.demo.controller;
 
-public class ProductController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibuy.product.demo.entity.Purchase;
+import com.ibuy.product.demo.service.PurchaseService;
+
+
+@RestController
+public class ProductController {
+	
+	
+	@Autowired
+	PurchaseService purchaseService;
+	
+	
+	@GetMapping("/history{pageSize}")
+	public List<Purchase> getPurchaseHistory(@PathVariable int pageSize){
+		return purchaseService.getPurchaseHistory(pageSize);
+	}
 }
